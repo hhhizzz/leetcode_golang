f62f469347e29eca1ff1f2cd503fef7e00750b69
xunix huang <qiwei_huang@qq.com>
2019-08-13 21:22:02 +0800

    add problem

---
+++ 547/main.go
@@ -0,0 +1,25 @@
+package _547
+
+func dfs(M *[][]int, found *[]bool, i int) {
+    if (*found)[i] {
+        return
+    }
+    (*found)[i] = true
+    for j := 0; j < len((*M)[i]); j++ {
+        if i != j && (*M)[i][j] == 1 {
+            dfs(M, found, j)
+        }
+    }
+}
+
+func findCircleNum(M [][]int) int {
+    found := make([]bool, len(M))
+    count := 0
+    for i := 0; i < len(M); i++ {
+        if !found[i] {
+            count++
+            dfs(&M, &found, i)
+        }
+    }
+    return count
+}
