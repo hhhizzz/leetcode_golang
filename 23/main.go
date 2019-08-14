f46d60bddb9243ec9a1e6c6d74fddfd7977ca64f
xunix huang <qiwei_huang@qq.com>
2019-08-14 21:30:46 +0800

    add 23

--- 23/main.go
+++ 23/main.go
@@ -1,1 +1,59 @@
-package _3
+package _23
+
+import "container/heap"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+    Val  int
+    Next *ListNode
+}
+
+type nodeHeap []*ListNode
+
+func (h nodeHeap) Len() int {
+    return len(h)
+}
+func (h nodeHeap) Less(i, j int) bool {
+    return h[i].Val < h[j].Val
+}
+func (h nodeHeap) Swap(i, j int) {
+    h[i], h[j] = h[j], h[i]
+}
+func (h *nodeHeap) Push(x interface{}) {
+    *h = append(*h, x.(*ListNode))
+}
+func (h *nodeHeap) Pop() interface{} {
+    old := *h
+    n := len(old)
+    x := old[n-1]
+    *h = old[:n-1]
+    return x
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+    h := &nodeHeap{}
+    heap.Init(h)
+    front := &ListNode{}
+    current := front
+    for i := 0; i < len(lists); i++ {
+        if lists[i] != nil {
+            heap.Push(h, lists[i])
+        }
+    }
+    for h.Len() != 0 {
+        min := (*h)[0]
+        current.Next = min
+        heap.Pop(h)
+        if min.Next != nil {
+            heap.Push(h, min.Next)
+        }
+        current = current.Next
+    }
+    return front.Next
+}
--- 23/main_test.go
+++ 23/main_test.go
@@ -1,1 +1,35 @@
 package _23
+
+import (
+    "strconv"
+    "testing"
+)
+
+func makeListNode(num []int) *ListNode {
+    head := ListNode{}
+    last := &head
+    for _, n := range num {
+        node := ListNode{n, nil}
+        last.Next = &node
+        last = &node
+    }
+    return head.Next
+}
+
+func toStr(l *ListNode) string {
+    result := "["
+    for l != nil {
+        result += strconv.Itoa(l.Val) + ", "
+        l = l.Next
+    }
+    result += "]"
+    return result
+}
+
+func TestMergeKLists(t *testing.T) {
+    list1 := makeListNode([]int{1, 4, 5})
+    list2 := makeListNode([]int{1, 3, 4})
+    list3 := makeListNode([]int{2, 6})
+    result := mergeKLists([]*ListNode{list1, list2, list3})
+    println(toStr(result))
+}
