20c30181021241eaa045f435b15d438aed45914e
xunix huang <qiwei_huang@qq.com>
2019-08-26 20:58:59 +0800

    add 55

--- 55/main.go
+++ 55/main.go
@@ -1,1 +1,11 @@
-package _5
+package _55
+
+func canJump(nums []int) bool {
+    lastPos := len(nums) - 1
+    for i := len(nums) - 2; i >= 0; i-- {
+        if nums[i]+i>=lastPos{
+            lastPos = i
+        }
+    }
+    return lastPos==0
+}
