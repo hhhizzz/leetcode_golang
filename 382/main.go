777c1e0d88bd161b5ee11318244912e8300f0e25
xunix huang <qiwei_huang@qq.com>
2019-09-03 20:18:04 +0800

    add 382

---
+++ 382/main.go
@@ -0,0 +1,40 @@
+package _382
+
+import "math/rand"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+    Val  int
+    Next *ListNode
+}
+
+type Solution struct {
+    head *ListNode
+}
+
+/** @param head The linked list's head.
+  Note that the head is guaranteed to be not null, so it contains at least one node. */
+func Constructor(head *ListNode) Solution {
+    return Solution{head: head}
+}
+
+/** Returns a random node's value. */
+func (this *Solution) GetRandom() int {
+    result := this.head.Val
+    current := this.head.Next
+    for k := 2; current != nil; k++ {
+        chance := 1 / float64(k)
+        happen := rand.Float64()
+        if happen < chance {
+            result = current.Val
+        }
+        current = current.Next
+    }
+    return result
+}
