2c0b354f98d3a0e454fe90cfa5863b79ccb21471
xunix huang <qiwei_huang@qq.com>
2019-09-04 22:01:15 +0800

    add 394

---
+++ 394/main.go
@@ -0,0 +1,33 @@
+package _394
+
+func decodeString(s string) string {
+    var result string
+    for i := 0; i < len(s); i++ {
+        c := s[i]
+        if c <= '9' && c >= '0' {
+            times := 0
+            for s[i] != '[' {
+                times *= 10
+                times += int(s[i] - '0')
+                i++
+            }
+            leftBucket := 1
+            j := i
+            for ; leftBucket != 0; j++ {
+                if s[j] == '[' {
+                    leftBucket++
+                } else if s[j] == ']' {
+                    leftBucket--
+                }
+            }
+            unit := decodeString(s[i+1 : j])
+            for time := 1; time <= times; time++ {
+                result += unit
+            }
+            i = j
+        } else {
+            result += string(c)
+        }
+    }
+    return result
+}
