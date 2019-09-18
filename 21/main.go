05d04f2465722cebe6f648ef3c3043da8912e445
xunix huang <qiwei_huang@qq.com>
2019-09-18 20:01:49 +0800

    upgrade 21

--- 21/main.go
+++ 21/main.go
@@ -6,6 +6,28 @@ type ListNode struct {
 }
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+    root := &ListNode{}
+    current := root
+    for l1 != nil && l2 != nil {
+        if l1.Val < l2.Val {
+            current.Next = l1
+            l1 = l1.Next
+        } else {
+            current.Next = l2
+            l2 = l2.Next
+        }
+        current = current.Next
+    }
+    if l1 == nil {
+        current.Next = l2
+    } else {
+        current.Next = l1
+    }
+    return root.Next
+}
+
+//比较优雅的写法
+func mergeTwoLists2(l1 *ListNode, l2 *ListNode) *ListNode {
     if l1 == nil {
         return l2
     }
