772b44ff48a6eb5904676aec6025850a5568b8e0
xunix huang <qiwei_huang@qq.com>
2019-09-18 19:51:19 +0800

    add 234

---
+++ 234/main.go
@@ -0,0 +1,20 @@
+package _234
+
+type ListNode struct {
+    Val  int
+    Next *ListNode
+}
+
+func isPalindrome(head *ListNode) bool {
+    var data []int
+    for head != nil {
+        data = append(data, head.Val)
+        head = head.Next
+    }
+    for i := 0; i < len(data)>>1; i++ {
+        if data[i] != data[len(data)-i] {
+            return false
+        }
+    }
+    return true
+}
