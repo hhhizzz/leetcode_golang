8dd739b837eec91c3bff25047783cfc09d04e0ec
xunix huang <qiwei_huang@qq.com>
2019-09-21 23:00:44 +0800

    add 240

---
+++ 240/main.go
@@ -0,0 +1,19 @@
+package _240
+
+func searchMatrix(matrix [][]int, target int) bool {
+    if len(matrix) == 0 || len(matrix[0]) == 0 {
+        return false
+    }
+    row := len(matrix) - 1
+    col := 0
+    for row >= 0 && col < len(matrix[0]) {
+        if matrix[row][col] == target {
+            return true
+        } else if target < matrix[row][col] {
+            row--
+        } else {
+            col++
+        }
+    }
+    return false
+}
