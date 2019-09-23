1456ac2679e2bad8326d86d2e579fa9397314e98
xunix huang <qiwei_huang@qq.com>
2019-09-23 22:24:03 +0800

    add 125

---
+++ 125/main_test.go
@@ -0,0 +1,11 @@
+package _125
+
+import (
+    "fmt"
+    "testing"
+)
+
+func TestIsPalindrome(t *testing.T) {
+    fmt.Println(isPalindrome("A man, a plan, a canal: Panama"))
+    fmt.Println(isPalindrome("0P"))
+}
