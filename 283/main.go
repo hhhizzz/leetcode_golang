de2d2e0f57e91fc0e79a1947f6a183a3ad70fe73
xunix huang <qiwei_huang@qq.com>
2019-09-30 20:49:11 +0800

    add 283

---
+++ 283/main.go
@@ -0,0 +1,16 @@
+package _283
+
+//看起来很简单的一道题，其实还是有点考验编码能力的
+func moveZeroes(nums []int) {
+    pivot := len(nums)
+    for i := 0; i < pivot; i++ {
+        if nums[i] == 0 {
+            pivot--
+            for j := i; j < pivot; j++ {
+                nums[j] = nums[j+1]
+            }
+            nums[pivot] = 0
+            i--
+        }
+    }
+}
