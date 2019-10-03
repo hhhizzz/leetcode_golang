65ef0006ed11e971c2129cfaaa664d11fc35e591
xunix huang <qiwei_huang@qq.com>
2019-10-03 20:01:50 +0800

    add 83

---
+++ 83/main.go
@@ -0,0 +1,21 @@
+package _83
+
+type ListNode struct {
+    Val  int
+    Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+    if head == nil {
+        return head
+    }
+    current := head
+    for current.Next != nil {
+        if current.Val == current.Next.Val {
+            current.Next = current.Next.Next
+        } else {
+            current = current.Next
+        }
+    }
+    return head
+}
