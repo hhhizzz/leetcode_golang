86817f96e49c47b8849a847b5fbfed339146638e
xunix huang <qiwei_huang@qq.com>
2019-10-10 23:32:37 +0800

    add 100

--- 100/main.go
+++ 100/main.go
@@ -1,1 +1,22 @@
-package _00
+package _100
+
+type TreeNode struct {
+    Val   int
+    Left  *TreeNode
+    Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    if p == nil || q == nil {
+        if p == nil && q == nil {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    if p.Val != q.Val {
+        return false
+    }
+    return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
