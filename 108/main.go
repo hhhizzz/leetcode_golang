de7455d12d86f680b9b5afc7f5d4ddc892201473
xunix huang <qiwei_huang@qq.com>
2019-10-16 19:57:55 +0800

    add 108

---
+++ 108/main.go
@@ -0,0 +1,19 @@
+package _108
+
+type TreeNode struct {
+    Val   int
+    Left  *TreeNode
+    Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+    if len(nums) <= 0 {
+        return nil
+    }
+    mid := len(nums) / 2
+    root := &TreeNode{Val: nums[mid]}
+    root.Left = sortedArrayToBST(nums[:mid])
+    root.Right = sortedArrayToBST(nums[mid+1:])
+
+    return root
+}
