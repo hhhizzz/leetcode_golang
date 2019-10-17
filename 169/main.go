35f3bceea22e69441e5d9bd1a193942e48cd49a2
xunix huang <qiwei_huang@qq.com>
2019-10-17 19:38:03 +0800

    upgrade 169

--- 169/main.go
+++ 169/main.go
@@ -1,17 +1,18 @@
 package _169
 
 func majorityElement(nums []int) int {
-    m := make(map[int]int)
-    n2 := len(nums) >> 1
-    for _,num := range nums {
+    m := map[int]int{}
+    half := len(nums) >> 1
+
+    for _, num := range nums {
         if _, ok := m[num]; ok {
-            m[num] += 1;
+            m[num] += 1
         } else {
-            m[num] = 1;
+            m[num] = 1
         }
-        if m[num] > n2 {
+        if m[num] > half {
             return num
         }
     }
-    return nums[len(nums)-1]
+    return -1
 }
