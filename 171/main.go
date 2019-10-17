a9fa96bb298071ebe96eccc956c0247fd3a1aac8
xunix huang <qiwei_huang@qq.com>
2019-10-17 23:41:58 +0800

    add 171

---
+++ 171/main.go
@@ -0,0 +1,13 @@
+package _171
+
+func titleToNumber(s string) int {
+    result := 1
+    for i, c := range s {
+        if i != 0 {
+            result *= 26
+            result += 1
+        }
+        result += int(c) - 'A'
+    }
+    return result
+}
