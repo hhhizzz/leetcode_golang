de62c1480cc4872af0e58aedfcbe6e23b6ed941f
xunix huang <qiwei_huang@qq.com>
2019-10-17 23:34:46 +0800

    add 387

---
+++ 387/main.go
@@ -0,0 +1,18 @@
+package _387
+
+func firstUniqChar(s string) int {
+    m := map[int32]int{}
+    for _, c := range s {
+        if _, ok := m[c]; ok {
+            m[c] += 1
+        } else {
+            m[c] = 1
+        }
+    }
+    for i, c := range s {
+        if m[c] == 1 {
+            return i
+        }
+    }
+    return -1
+}
