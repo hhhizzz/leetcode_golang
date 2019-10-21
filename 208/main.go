9db016c6fd4c21c5cff1e4f2142bb3244c12363a
xunix huang <qiwei_huang@qq.com>
2019-10-21 22:03:45 +0800

    upgrade 208

--- 208/main.go
+++ 208/main.go
@@ -1,49 +1,54 @@
 package _208
 
 type Trie struct {
-    data map[int32]*Trie
-    cut  bool
+    pre *letter
+}
+
+type letter struct {
+    c        int32
+    isLetter bool
+    next     map[int32]*letter
 }
 
 /** Initialize your data structure here. */
 func Constructor() Trie {
-    trie := Trie{data: map[int32]*Trie{}}
+    trie := Trie{pre: &letter{0, false, map[int32]*letter{}}}
     return trie
 }
 
 /** Inserts a word into the trie. */
 func (this *Trie) Insert(word string) {
-    current := this
+    current := this.pre
     for _, c := range word {
-        if next, ok := current.data[c]; ok {
-            current = next
+        if nextLetter, ok := current.next[c]; ok {
+            current = nextLetter
         } else {
-            current.data[c] = &Trie{data: map[int32]*Trie{}}
-            current = current.data[c]
+            current.next[c] = &letter{c, false, map[int32]*letter{}}
+            current = current.next[c]
         }
     }
-    current.cut = true
+    current.isLetter = true
 }
 
 /** Returns if the word is in the trie. */
 func (this *Trie) Search(word string) bool {
-    current := this
+    current := this.pre
     for _, c := range word {
-        if next, ok := current.data[c]; ok {
-            current = next
+        if nextLetter, ok := current.next[c]; ok {
+            current = nextLetter
         } else {
             return false
         }
     }
-    return current.cut
+    return current.isLetter
 }
 
 /** Returns if there is any word in the trie that starts with the given prefix. */
 func (this *Trie) StartsWith(prefix string) bool {
-    current := this
+    current := this.pre
     for _, c := range prefix {
-        if next, ok := current.data[c]; ok {
-            current = next
+        if nextLetter, ok := current.next[c]; ok {
+            current = nextLetter
         } else {
             return false
         }
