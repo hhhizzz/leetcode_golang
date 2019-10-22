ea517ba04c97c3c83c40f67213e38d4448a548cf
xunix huang <qiwei_huang@qq.com>
2019-10-22 20:38:11 +0800

    add 349

---
+++ 349/main.go
@@ -0,0 +1,28 @@
+package _349
+
+func intersection(nums1 []int, nums2 []int) []int {
+    m1 := map[int]bool{}
+    m2 := map[int]bool{}
+    for _, i := range nums1 {
+        m1[i] = true
+    }
+    for _, i := range nums2 {
+        m2[i] = true
+    }
+    mResult := map[int]bool{}
+    for key := range m1 {
+        if _, ok := m2[key]; ok {
+            mResult[key] = true
+        }
+    }
+    for key := range m2 {
+        if _, ok := m1[key]; ok {
+            mResult[key] = true
+        }
+    }
+    var result []int
+    for key := range mResult {
+        result = append(result, key)
+    }
+    return result
+}
