4a1ef665b35aa7ee9148ffeafb8e8f6856cf6e25
xunix huang <qiwei_huang@qq.com>
2019-10-23 20:57:48 +0800

    add 378

---
+++ 378/main.go
@@ -0,0 +1,38 @@
+package _378
+
+func findSmall(matrix [][]int, mid int) (int, bool) {
+    count := 0
+    contain := false
+    for i := 0; i < len(matrix); i++ {
+        for j := 0; j < len(matrix[i]); j++ {
+            if matrix[i][j] <= mid {
+                if matrix[i][j] == mid {
+                    contain = true
+                }
+                count++
+            } else {
+                break
+            }
+        }
+    }
+    return count, contain
+}
+
+func kthSmallest(matrix [][]int, k int) int {
+    n := len(matrix)
+    left := matrix[0][0]
+    right := matrix[n-1][n-1]
+
+    for left < right {
+        mid := (left + right) >> 1
+        num, contain := findSmall(matrix, mid)
+        if num > k || (num == k && !contain) {
+            right = mid
+        } else if num == k && contain {
+            return mid
+        } else {
+            left = mid + 1
+        }
+    }
+    return left
+}
---
+++ 378/main_test.go
@@ -0,0 +1,11 @@
+package _378
+
+import "testing"
+
+func TestKthSmallest(t *testing.T) {
+    kthSmallest([][]int{
+        {1, 5, 9},
+        {10, 11, 13},
+        {12, 13, 15},
+    }, 8)
+}
