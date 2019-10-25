21f5f3e8e61dc931f266802ed7c1c80fc24d044b
xunix huang <qiwei_huang@qq.com>
2019-10-25 23:43:45 +0800

    upgrade 239

--- 239/main.go
+++ 239/main.go
@@ -1,5 +1,6 @@
 package _239
 
+//双端队列法 window是双端队列，第一个元素表示当前位置到k之前的位置最大的元素，每次往后迭代的时候，新进的元素向队列左端推进直到到前一个元素比它大或者到了最先一个元素
 func maxSlidingWindow2(nums []int, k int) []int {
     if len(nums) == 0 {
         return []int{}
@@ -27,6 +28,7 @@ func max(a, b int) int {
     return a
 }
 
+//方法1： 构建两个数组，分别表示每隔k的范围内最大值，最后出结果的时候找需要范围的两边即可
 func maxSlidingWindow(nums []int, k int) []int {
     if len(nums) == 0 {
         return nums
