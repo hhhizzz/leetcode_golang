44d2c40f0530efa7d71e07393b71d0fde65318ee
xunix huang <qiwei_huang@qq.com>
2019-11-04 00:26:22 +0800

    add 73

---
+++ 73/main_test.go
@@ -0,0 +1,13 @@
+package _73
+
+import "testing"
+
+func TestSetZeroz(t *testing.T) {
+    setZeroes([][]int{
+        {0, 1, 2, 0},
+        {3, 4, 5, 2},
+        {1, 3, 1, 5},
+    },
+    )
+
+}
