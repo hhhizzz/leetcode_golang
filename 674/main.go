ee4c249a21fcdb9b2172d550d29e899345e77612
xunix huang <qiwei_huang@qq.com>
2019-11-04 23:40:57 +0800

    add 674

---
+++ 674/main.go
@@ -0,0 +1,17 @@
+package _674
+
+func findLengthOfLCIS(nums []int) int {
+    result := 0
+    current := 0
+    for i := 0; i < len(nums); i++ {
+        if i == 0 || nums[i] > nums[i-1] {
+            current += 1
+            if current > result {
+                result = current
+            }
+        } else {
+            current = 1
+        }
+    }
+    return result
+}
