e2803d3f0346c352f0fb33a7f8fcfafe46e945ad
xunix huang <qiwei_huang@qq.com>
2019-11-05 23:07:51 +0800

    upgrade 151

--- 151/main.go
+++ 151/main.go
@@ -1,1 +1,24 @@
-package _51
+package _151
+
+import (
+    "strings"
+)
+
+func reverseWords(s string) string {
+    splits := strings.Split(s, " ")
+    var resultArray []string
+    for _, split := range splits {
+        if split == "" {
+            continue
+        }
+        resultArray = append(resultArray, split)
+    }
+    var result string
+    for i := len(resultArray) - 1; i >= 0; i-- {
+        result += resultArray[i]
+        if i != 0 {
+            result += " "
+        }
+    }
+    return result
+}
--- 151/main_test.go
+++ 151/main_test.go
@@ -1,1 +1,11 @@
 package _151
+
+import (
+    "fmt"
+    "testing"
+)
+
+func TestReverseWords(t *testing.T) {
+    words := reverseWords("  hello  world!  ")
+    fmt.Println(words)
+}
