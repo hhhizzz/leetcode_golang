f8bcec8b900f60a00575354ad9ece88043b33041
xunix huang <qiwei_huang@qq.com>
2019-11-05 23:07:41 +0800

    add 361

---
+++ 361/main.go
@@ -0,0 +1,77 @@
+package _361
+
+func maxKilledEnemies(grid [][]byte) int {
+    if len(grid) == 0 {
+        return 0
+    }
+    m := len(grid)
+    n := len(grid[0])
+    bomb := make([][]int, m)
+    for i := 0; i < m; i++ {
+        bomb[i] = make([]int, n)
+    }
+    result := 0
+
+    for i := 0; i < m; i++ {
+        //to right
+        current := 0
+        for j := n - 1; j >= 0; j-- {
+            if grid[i][j] == '0' {
+                bomb[i][j] += current
+                if bomb[i][j] > result {
+                    result = bomb[i][j]
+                }
+            } else if grid[i][j] == 'E' {
+                current++
+            } else if grid[i][j] == 'W' {
+                current = 0
+            }
+        }
+        //to left
+        current = 0
+        for j := 0; j < n; j++ {
+            if grid[i][j] == '0' {
+                bomb[i][j] += current
+                if bomb[i][j] > result {
+                    result = bomb[i][j]
+                }
+            } else if grid[i][j] == 'E' {
+                current++
+            } else if grid[i][j] == 'W' {
+                current = 0
+            }
+        }
+    }
+    for j := 0; j < n; j++ {
+        //to up
+        current := 0
+        for i := 0; i < m; i++ {
+            if grid[i][j] == '0' {
+                bomb[i][j] += current
+                if bomb[i][j] > result {
+                    result = bomb[i][j]
+                }
+            } else if grid[i][j] == 'E' {
+                current++
+            } else if grid[i][j] == 'W' {
+                current = 0
+            }
+        }
+        //to down
+        current = 0
+        for i := m - 1; i >= 0; i-- {
+            if grid[i][j] == '0' {
+                bomb[i][j] += current
+                if bomb[i][j] > result {
+                    result = bomb[i][j]
+                }
+            } else if grid[i][j] == 'E' {
+                current++
+            } else if grid[i][j] == 'W' {
+                current = 0
+            }
+        }
+    }
+
+    return result
+}
