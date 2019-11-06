f7c546faeaf0e5cadeefaffe6edeca08be19eb58
xunix huang <qiwei_huang@qq.com>
2019-11-06 23:47:28 +0800

    add 393

---
+++ 393/main_test.go
@@ -0,0 +1,18 @@
+package _393
+
+import (
+    "fmt"
+    "testing"
+)
+
+func TestUtf(t *testing.T) {
+    var result bool
+    result = validUtf8([]int{197, 130, 1})
+    fmt.Println(result)
+    result = validUtf8([]int{235, 140, 4})
+    fmt.Println(result)
+    result = validUtf8([]int{237})
+    fmt.Println(result)
+    result = validUtf8([]int{240, 162, 138, 147})
+    fmt.Println(result)
+}
