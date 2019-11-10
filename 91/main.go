c0ce9ee654946e9f416e86ba95d4bd57c565028d
xunix huang <qiwei_huang@qq.com>
2019-11-10 21:04:29 +0800

    add 91

---
+++ 91/main.go
@@ -0,0 +1,50 @@
+package _91
+
+func numDecodings(s string) int {
+    dp := make([]int, len(s))
+    if s[0] > '0' {
+        dp[0] = 1
+    } else {
+        return 0
+    }
+    for i := 1; i < len(s); i++ {
+        if s[i-1] == '1' {
+            if s[i] > '0' {
+                if i-2 >= 0 {
+                    dp[i] = dp[i-1] + dp[i-2]
+                } else {
+                    dp[i] = dp[i-1] + 1
+                }
+            } else {
+                if i-2 >= 0 {
+                    dp[i] = dp[i-2]
+                } else {
+                    dp[i] = 1
+                }
+            }
+        } else if s[i-1] == '2' {
+            if s[i] <= '6' && s[i] > '0' {
+                if i-2 >= 0 {
+                    dp[i] = dp[i-1] + dp[i-2]
+                } else {
+                    dp[i] = dp[i-1] + 1
+                }
+            } else if s[i] == '0' {
+                if i-2 >= 0 {
+                    dp[i] = dp[i-2]
+                } else {
+                    dp[i] = 1
+                }
+            } else {
+                dp[i] = dp[i-1]
+            }
+        } else {
+            if s[i] == '0' {
+                return 0
+            } else {
+                dp[i] = dp[i-1]
+            }
+        }
+    }
+    return dp[len(s)-1]
+}
---
+++ 91/main_test.go
@@ -0,0 +1,18 @@
+package _91
+
+import (
+    "fmt"
+    "testing"
+)
+
+func TestNumDecodings(t *testing.T) {
+    fmt.Println(numDecodings("1"))
+    fmt.Println(numDecodings("10"))
+    fmt.Println(numDecodings("26"))
+    fmt.Println(numDecodings("27"))
+    fmt.Println(numDecodings("100"))
+    fmt.Println(numDecodings("110"))
+    fmt.Println(numDecodings("210"))
+    fmt.Println(numDecodings("120"))
+    fmt.Println(numDecodings("2262312412"))
+}
