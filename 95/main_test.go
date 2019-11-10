57dda5a1255aa716049440f20c612c95cb356262
xunix huang <qiwei_huang@qq.com>
2019-11-10 21:39:16 +0800

    add 95

---
+++ 95/main_test.go
@@ -0,0 +1,35 @@
+package _95
+
+import (
+    "fmt"
+    "testing"
+)
+
+func (node *TreeNode) toString() string {
+    if node == nil {
+        return ""
+    }
+    result := fmt.Sprintf("{%d}", node.Val)
+    result += node.Left.toString()
+    result += node.Right.toString()
+    return result
+}
+
+func TestGenerateTrees(t *testing.T) {
+    var nodes []*TreeNode
+    nodes = generateTrees(2)
+    for _, node := range nodes {
+        fmt.Println(node.toString() + ", ")
+    }
+    fmt.Println()
+    nodes = generateTrees(3)
+    for _, node := range nodes {
+        fmt.Println(node.toString() + ", ")
+    }
+    fmt.Println()
+    nodes = generateTrees(4)
+    for _, node := range nodes {
+        fmt.Println(node.toString() + ", ")
+    }
+    fmt.Println()
+}
