bc8f133459696ca011938b38409d77e7eaf7630a
xunix huang <qiwei_huang@qq.com>
2019-11-10 15:34:47 +0800

    upgrade 93

--- 93/main.go
+++ 93/main.go
@@ -1,45 +1,41 @@
 package _93
 
-import "strconv"
+import (
+    "strconv"
+)
 
-func checkIp(bytes []byte) bool {
-    if len(bytes) > 1 && bytes[0] == '0' {
-        return false
-    }
-    number, _ := strconv.Atoi(string(bytes))
-    if number > 255 {
-        return false
-    }
-    return true
-}
-
-func dfs(bytes []byte, left int, result *[]string, current string) {
-    if left == 1 {
-        if !checkIp(bytes) {
-            return
-        } else {
-            current += string(bytes)
-            *result = append(*result, current)
-            return
-        }
-    } else {
-        for i := 1; i <= 3 && i < len(bytes); i++ {
-            if checkIp(bytes[:i]) {
-                next := current
-                number, _ := strconv.Atoi(string(bytes[:i]))
-                if number > 255 {
-                    break
-                }
-                next += string(bytes[:i])
-                next += "."
-                dfs(bytes[i:], left-1, result, next)
+func dfs(s string, current []string, result *[]string) {
+    if len(current) == 4 && len(s) == 0 {
+        ip := ""
+        for i, c := range current {
+            ip += c
+            if i != 3 {
+                ip += "."
             }
+        }
+        *result = append(*result, ip)
+    }
+    if len(current) > 4 || len(s) == 0 {
+        return
+    }
+    for i := 1; i <= len(s) && i <= 3; i++ {
+        cutString := s[:i]
+        if len(cutString) > 1 && cutString[0] == '0' {
+            break
+        } else {
+            cut, _ := strconv.Atoi(cutString)
+            if cut > 255 {
+                break
+            }
+            current = append(current, cutString)
+            dfs(s[i:], current, result)
+            current = current[:len(current)-1]
         }
     }
 }
 
 func restoreIpAddresses(s string) []string {
     var result []string
-    dfs([]byte(s), 4, &result, "")
+    dfs(s, []string{}, &result)
     return result
 }
