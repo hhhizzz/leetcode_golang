1dcfc4bd27d619f1d7b9bb591a505ce909f4adb3
xunix huang <qiwei_huang@qq.com>
2019-11-19 21:14:26 +0800

    add 111

--- 111/main.go
+++ 111/main.go
@@ -6,6 +6,28 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func minDepth(root *TreeNode) int {
 
+	if root == nil {
+		return 0
+	}
+	left := minDepth(root.Left)
+	right := minDepth(root.Right)
+	if left == 0 && right == 0 {
+		return 1
+	} else {
+		if left == 0 {
+			return 1 + right
+		} else if right == 0 {
+			return 1 + left
+		}
+		return 1 + min(left, right)
+	}
 }
