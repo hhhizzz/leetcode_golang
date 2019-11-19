36563eb9432bc6e72729fe2621e0d36f9ef80206
xunix huang <qiwei_huang@qq.com>
2019-11-19 21:33:40 +0800

    add 167

---
+++ 167/main.go
@@ -0,0 +1,17 @@
+package _167
+
+func twoSum(numbers []int, target int) []int {
+    left := 0
+    right := len(numbers) - 1
+    for left < right {
+        sum := numbers[left] + numbers[right]
+        if sum == target {
+            return []int{left + 1, right + 1}
+        } else if sum < target {
+            left++
+        } else {
+            right--
+        }
+    }
+    return []int{}
+}
