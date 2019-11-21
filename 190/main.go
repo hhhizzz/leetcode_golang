7121168e22adaa3f7e7e46290eb3ea880ed165bd
xunix huang <qiwei_huang@qq.com>
2019-11-21 23:25:34 +0800

    add 190

---
+++ 190/main.go
@@ -0,0 +1,17 @@
+package _190
+
+func reverseBits(num uint32) uint32 {
+    bytes := make([]uint32, 32)
+    for i := 0; i < 32; i++ {
+        bytes[i] = num % 2
+        num /= 2
+    }
+
+    var result uint32 = 0
+    var bit uint32 = 1
+    for i := 31; i >= 0; i-- {
+        result += bit * bytes[i]
+        bit *= 2
+    }
+    return result
+}
