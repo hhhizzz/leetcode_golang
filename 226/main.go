f03aeb3c9de856aa7cda555cbfd09a2b24479080
xunix huang <qiwei_huang@qq.com>
2019-11-22 21:20:55 +0800

    add 226

---
+++ 226/main.go
@@ -0,0 +1,17 @@
+package _226
+
+type TreeNode struct {
+    Val   int
+    Left  *TreeNode
+    Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+    if root == nil {
+        return root
+    }
+    root.Left, root.Right = root.Right, root.Left
+    invertTree(root.Left)
+    invertTree(root.Right)
+    return root
+}
