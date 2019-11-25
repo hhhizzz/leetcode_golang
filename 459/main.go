e168cfcdcc6795c0596074e54585c9c4f69dcc95
xunix huang <qiwei_huang@qq.com>
2019-11-25 23:35:33 +0800

    add 459

---
+++ 459/main.go
@@ -0,0 +1,29 @@
+package _459
+
+import "strings"
+
+//暴力匹配
+func repeatedSubstringPattern1(s string) bool {
+    for i := 1; i <= len(s)>>1; i++ {
+        if len(s)%i != 0 {
+            continue
+        }
+        t := i
+        for ; t < len(s) && s[t] == s[t%i]; t++ {
+
+        }
+        if t == len(s) {
+            return true
+        }
+    }
+    return false
+}
+
+func repeatedSubstringPattern(s string) bool {
+    s2 := s + s
+    s2 = s2[1 : len(s2)-1]
+    if strings.Contains(s2, s) {
+        return true
+    }
+    return false
+}
