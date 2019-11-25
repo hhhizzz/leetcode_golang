7530302b02e8030a3a1e6dbff20fb75c432c570e
xunix huang <qiwei_huang@qq.com>
2019-11-25 23:06:06 +0800

    add 876

---
+++ 876/main.go
@@ -0,0 +1,16 @@
+package _876
+
+type ListNode struct {
+    Val  int
+    Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+    slow := head
+    fast := head
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+    return slow
+}
