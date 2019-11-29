016832b126705af4a1538a2009240ef6d423d5df
xunix huang <qiwei_huang@qq.com>
2019-11-29 23:44:44 +0800

    add 1089

---
+++ 1089/main.go
@@ -0,0 +1,14 @@
+package _1089
+
+func duplicateZeros(arr []int) {
+    for i := 0; i < len(arr); i++ {
+        if i != len(arr)-1 && arr[i] == 0 {
+            temp := arr[i+1]
+            arr[i+1] = 0
+            for j := i + 2; j < len(arr); j++ {
+                arr[j], temp = temp, arr[j]
+            }
+            i++
+        }
+    }
+}
