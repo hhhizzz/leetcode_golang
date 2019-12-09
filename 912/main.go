ccfb8cd7dd4594d2b7e6dbbbf0662a6452910185
xunix huang <qiwei_huang@qq.com>
2019-12-09 22:53:12 +0800

    add 912

---
+++ 912/main.go
@@ -0,0 +1,18 @@
+package _912
+
+//这道题给定了数据的大小范围，明显是想考察基数排序
+func sortArray(nums []int) []int {
+    temp := make([]int, 100001)
+    for i := 0; i < len(nums); i++ {
+        temp[nums[i]+50000] += 1
+    }
+    result := make([]int, len(nums))
+    idx := 0
+    for i := 0; i < len(temp); i++ {
+        for j := 0; j < temp[i]; j++ {
+            result[idx] = i - 50000
+            idx++
+        }
+    }
+    return result
+}
