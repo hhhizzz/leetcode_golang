cef1441f7581837a427b5164c8b9df34f386fd21
xunix huang <qiwei_huang@qq.com>
2019-12-27 23:12:54 +0800

    add 257

---
+++ 257/main.go
@@ -0,0 +1,31 @@
+package _257
+
+import "strconv"
+
+type TreeNode struct {
+    Val   int
+    Left  *TreeNode
+    Right *TreeNode
+}
+
+func dfs(realRoot bool, root *TreeNode, current string, result *[]string) {
+    if root == nil {
+        return
+    }
+    if !realRoot {
+        current += "->"
+    }
+
+    current += strconv.Itoa(root.Val)
+    if root.Left == nil && root.Right == nil {
+        *result = append(*result, current)
+    }
+    dfs(false, root.Left, current, result)
+    dfs(false, root.Right, current, result)
+}
+
+func binaryTreePaths(root *TreeNode) []string {
+    var result []string
+    dfs(true, root, "", &result)
+    return result
+}
