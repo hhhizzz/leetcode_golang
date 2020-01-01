fc957469dae60b1546e934d6d59d4748d261e711
xunix huang <qiwei_huang@qq.com>
2020-01-01 21:43:09 +0800

    add 209

---
+++ 209/main.go
@@ -0,0 +1,37 @@
+package _209
+
+import "math"
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func minSubArrayLen(s int, nums []int) int {
+    left := 0
+    right := 0
+    sum := 0
+    result := math.MaxInt32
+    for right < len(nums) {
+        sum += nums[right]
+        if sum >= s {
+            result = min(result, right-left+1)
+            for left < right {
+                sum -= nums[left]
+                left++
+                if sum >= s {
+                    result = min(result, right-left+1)
+                } else {
+                    break
+                }
+            }
+        }
+        right++
+    }
+    if result == math.MaxInt32 {
+        return 0
+    }
+    return result
+}
