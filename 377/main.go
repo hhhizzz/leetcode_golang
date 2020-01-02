66664feaa94ddf2ece4e1a64c774a87a92e7e783
xunix huang <qiwei_huang@qq.com>
2020-01-02 22:50:52 +0800

    add 377

---
+++ 377/main.go
@@ -0,0 +1,16 @@
+package _377
+
+func combinationSum4(nums []int, target int) int {
+    //dp[i]表示加到i有多少种可能，dp[i] = sum(dp[i-num]) 其中num是nums中的数
+    dp := make([]int, target+1)
+    for current := 1; current <= target; current++ {
+        for _, num := range nums {
+            if num == current {
+                dp[current] += 1
+            } else if num < current {
+                dp[current] += dp[current-num]
+            }
+        }
+    }
+    return dp[target]
+}
