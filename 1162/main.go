c6151f8e3a2fe6af82fc8a083f6700f9c0c5f536
xunix huang <qiwei_huang@qq.com>
2020-01-07 20:37:33 +0800

    add 1162

---
+++ 1162/main.go
@@ -0,0 +1,42 @@
+package _1162
+
+func maxDistance(grid [][]int) int {
+    var q [][2]int
+    for i := 0; i < len(grid); i++ {
+        for j := 0; j < len(grid[i]); j++ {
+            if grid[i][j] == 1 {
+                q = append(q, [2]int{i, j})
+            }
+        }
+    }
+    if len(q) == 0 || len(q) == len(grid)*len(grid[0]) {
+        return -1
+    }
+    result := 0
+    for len(q) != 0 {
+        current := q[0]
+        q = q[1:]
+        i := current[0]
+        j := current[1]
+        if i > 0 && grid[i-1][j] == 0 {
+            grid[i-1][j] = grid[i][j] + 1
+            q = append(q, [2]int{i - 1, j})
+        }
+        if j > 0 && grid[i][j-1] == 0 {
+            grid[i][j-1] = grid[i][j] + 1
+            q = append(q, [2]int{i, j - 1})
+        }
+        if i < len(grid)-1 && grid[i+1][j] == 0 {
+            grid[i+1][j] = grid[i][j] + 1
+            q = append(q, [2]int{i + 1, j})
+        }
+        if j < len(grid[0])-1 && grid[i][j+1] == 0 {
+            grid[i][j+1] = grid[i][j] + 1
+            q = append(q, [2]int{i, j + 1})
+        }
+        if grid[i][j] > result {
+            result = grid[i][j]
+        }
+    }
+    return result - 1
+}
