44db5a1a084068c8c0f8a86f4f57a5b920133c04
xunix huang <qiwei_huang@qq.com>
2020-01-08 21:43:44 +0800

    add 647

---
+++ 647/main.go
@@ -0,0 +1,24 @@
+package _647
+
+func countPalindromic(array []byte, i, j int) int {
+    count := 0
+    for i >= 0 && j < len(array) && array[i] == array[j] {
+        count += 1
+        //fmt.Println(string(array[i:j+1]))
+        i--
+        j++
+    }
+    return count
+}
+
+func countSubstrings(s string) int {
+    array := []byte(s)
+    result := 0
+    for i := 0; i < len(array); i++ {
+        result += countPalindromic(array, i, i)
+        if i+1 < len(array) {
+            result += countPalindromic(array, i, i+1)
+        }
+    }
+    return result
+}
