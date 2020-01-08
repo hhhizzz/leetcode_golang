47cd9fa7d403ce47ae799efac04749a92fa1f1f1
xunix huang <qiwei_huang@qq.com>
2020-01-08 20:23:39 +0800

    add 253

---
+++ 253/main_test.go
@@ -0,0 +1,12 @@
+package _253
+
+import (
+    "fmt"
+    "testing"
+)
+
+func TestMinMeetingRooms(t *testing.T) {
+    var result int
+    result = minMeetingRooms([][]int{{7, 10}, {2, 4}})
+    fmt.Println(result)
+}
