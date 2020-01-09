9d3f68003975e6412d3730f95f6a4189f1d87580
xunix huang <qiwei_huang@qq.com>
2020-01-09 21:21:59 +0800

    add 448

---
+++ 448/main.go
@@ -0,0 +1,17 @@
+package _448
+
+func findDisappearedNumbers(nums []int) []int {
+    var result []int
+    for i := 0; i < len(nums); i++ {
+        for nums[nums[i]-1] != nums[i] && nums[i] != i+1 {
+            nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+        }
+    }
+    //fmt.Println(nums)
+    for i := 0; i < len(nums); i++ {
+        if nums[i] != i+1 {
+            result = append(result, i+1)
+        }
+    }
+    return result
+}
