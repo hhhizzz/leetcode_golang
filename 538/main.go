d32ce9fc34a09300ec86e33484c12041b6a92db2
xunix huang <qiwei_huang@qq.com>
2020-01-10 17:50:54 +0800

    add 538

---
+++ 538/main.go
@@ -0,0 +1,33 @@
+package _538
+
+type TreeNode struct {
+    Val   int
+    Left  *TreeNode
+    Right *TreeNode
+}
+
+func inOrder(root *TreeNode, sum *int) {
+    if root == nil {
+        return
+    }
+    inOrder(root.Left, sum)
+    *sum -= root.Val
+    root.Val += *sum
+    inOrder(root.Right, sum)
+}
+
+func findSum(root *TreeNode, sum *int) {
+    if root == nil {
+        return
+    }
+    *sum += root.Val
+    findSum(root.Left, sum)
+    findSum(root.Right, sum)
+}
+
+func convertBST(root *TreeNode) *TreeNode {
+    sum := 0
+    findSum(root, &sum)
+    inOrder(root, &sum)
+    return root
+}
