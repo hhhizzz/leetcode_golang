3e7b1e4c159c3d64219e273cfcdf17d4af8ef844
xunix huang <qiwei_huang@qq.com>
2020-01-15 22:41:27 +0800

    add 339

---
+++ 339/main.go
@@ -0,0 +1,43 @@
+package _339
+
+type NestedInteger struct{}
+
+func (NestedInteger) IsInteger() bool {
+    panic("implement me")
+}
+
+func (NestedInteger) GetInteger() int {
+    panic("implement me")
+}
+
+func (NestedInteger) SetInteger(value int) {
+    panic("implement me")
+}
+
+func (NestedInteger) Add(elem NestedInteger) {
+    panic("implement me")
+}
+
+func (NestedInteger) GetList() []*NestedInteger {
+    panic("implement me")
+}
+
+func dfs(integer *NestedInteger, level int) int {
+    if integer.IsInteger() {
+        return level * integer.GetInteger()
+    } else {
+        result := 0
+        for _, next := range integer.GetList() {
+            result += dfs(next, level+1)
+        }
+        return result
+    }
+}
+
+func depthSum(nestedList []*NestedInteger) int {
+    result := 0
+    for _, nestedInteger := range nestedList {
+        result += dfs(nestedInteger, 1)
+    }
+    return result
+}
