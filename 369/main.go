dbfbe3f5ece0958600509a4e89637ece585a112f
xunix huang <qiwei_huang@qq.com>
2020-01-16 22:08:35 +0800

    add 369

---
+++ 369/main.go
@@ -0,0 +1,45 @@
+package _369
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reverse(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {
+        return head
+    }
+
+    prev, current := head, head.Next
+    for current != nil {
+        next := current.Next
+        current.Next = prev
+
+        prev, current = current, next
+    }
+    head.Next = nil
+    return prev
+}
+
+func plusOne(head *ListNode) *ListNode {
+    head = reverse(head)
+    current := head
+
+    carry := 1
+    for current.Next != nil {
+        current.Val += carry
+        carry = current.Val / 10
+        current.Val %= 10
+        current = current.Next
+    }
+    current.Val += carry
+    carry = current.Val / 10
+    current.Val %= 10
+    if carry != 0 {
+        current.Next = &ListNode{Val: carry}
+    }
+    return reverse(head)
+}
