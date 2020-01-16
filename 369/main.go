e9b12dccc09fa487c2c756ece91da7cc460cf3b6
xunix huang <qiwei_huang@qq.com>
2020-01-16 22:09:14 +0800

    upgrade 369

--- 369/main.go
+++ 369/main.go
@@ -1,12 +1,9 @@
 package _369
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+type ListNode struct {
+    Val  int
+    Next *ListNode
+}
 
 func reverse(head *ListNode) *ListNode {
     if head == nil || head.Next == nil {
