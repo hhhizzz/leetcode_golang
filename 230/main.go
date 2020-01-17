ef07085fa4e4542cf8871162e0b75d7f47259614
xunix huang <qiwei_huang@qq.com>
2020-01-17 19:36:48 +0800

    upgrade 230

--- 230/main.go
+++ 230/main.go
@@ -23,8 +23,35 @@ func dfs(root *TreeNode, k *int, ans *int) {
     dfs(root.Right, k, ans)
 }
 
-func kthSmallest(root *TreeNode, k int) int {
+func kthSmallest1(root *TreeNode, k int) int {
     var result int
     dfs(root, &k, &result)
     return result
+}
+
+//非递归中序的方案
+func kthSmallest(root *TreeNode, k int) int {
+    var stack []*TreeNode
+    current := root
+    for current != nil {
+        stack = append(stack, current)
+        current = current.Left
+    }
+    for len(stack) != 0 {
+        current := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        k--
+
+        //fmt.Println(current.Val)
+        if k == 0 {
+            return current.Val
+        }
+
+        current = current.Right
+        for current != nil {
+            stack = append(stack, current)
+            current = current.Left
+        }
+    }
+    return -1
 }
