f368faa1ba1aebb941af1ba656f8044a317c9abb
xunix huang <qiwei_huang@qq.com>
2020-02-01 01:26:07 +0800

    add 186

---
+++ 186/main.go
@@ -0,0 +1,28 @@
+package _186
+
+func reverseWords(s []byte) {
+    var words [][]byte
+    start := 0
+    end := 0
+    for end < len(s) {
+        for end < len(s) && s[end] != ' ' {
+            end++
+        }
+        temp := make([]byte, end-start)
+        copy(temp, s[start:end])
+        words = append(words, temp)
+        end++
+        start = end
+    }
+    pos := 0
+    for i := len(words) - 1; i >= 0; i-- {
+        for j := 0; j < len(words[i]); j++ {
+            s[pos] = words[i][j]
+            pos++
+        }
+        if pos < len(s) {
+            s[pos] = ' '
+            pos++
+        }
+    }
+}
