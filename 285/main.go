f7f764efc121f5bc7117c9655397f375f37f861f
xunix huang <qiwei_huang@qq.com>
2020-02-03 22:50:55 +0800

    add 285

---
+++ 285/main.go
@@ -0,0 +1,36 @@
+package _285
+
+type TreeNode struct {
+    Val   int
+    Left  *TreeNode
+    Right *TreeNode
+}
+
+func inorderSuccessor(root *TreeNode, p *TreeNode) *TreeNode {
+    s := []*TreeNode{}
+    current := root
+    for current != nil {
+        s = append(s, current)
+        current = current.Left
+    }
+    flag := false
+    for len(s) != 0 {
+        current = s[len(s)-1]
+        s = s[:len(s)-1]
+
+        if flag {
+            return current
+        }
+        if !flag && current == p {
+            flag = true
+        }
+        if current.Right != nil {
+            current = current.Right
+            for current != nil {
+                s = append(s, current)
+                current = current.Left
+            }
+        }
+    }
+    return nil
+}
