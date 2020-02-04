210bdea10f2baf43d36aa33d3805cc1fc3140128
xunix huang <qiwei_huang@qq.com>
2020-02-04 12:19:13 +0800

    add 198

--- 198/main.go
+++ 198/main.go
@@ -9,11 +9,15 @@ func max(a, b int) int {
 
 func rob(nums []int) int {
     dp := make([]int, len(nums))
+
     result := 0
-    for i := range nums {
-        dp[i] = nums[i]
-        for j := i - 2; j >= 0; j-- {
-            dp[i] = max(dp[i], dp[j]+nums[i])
+    for i := 0; i < len(dp); i++ {
+        if i == 0 {
+            dp[i] = nums[i]
+        } else if i == 1 {
+            dp[i] = max(dp[0], nums[i])
+        } else {
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
         }
         result = max(result, dp[i])
     }
