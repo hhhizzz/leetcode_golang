daf0309af19fcec15aa48165e2b3446e5d98da15
xunix huang <qiwei_huang@qq.com>
2020-02-06 18:41:06 +0800

    upgrade 974

--- 974/main.go
+++ 974/main.go
@@ -1,26 +1,21 @@
 package _974
 
 func subarraysDivByK(A []int, K int) int {
-    var P int
-    m := map[int]int{0: 1}
     result := 0
+    sumMod := 0
+
+    modCount := make([]int, K)
+    modCount[0] = 1
     for i := 0; i < len(A); i++ {
-        if i == 0 {
-            P = A[i] % K
-        } else {
-            P = (P + A[i]) % K
+        sumMod += A[i]
+        sumMod %= K
+
+        //由于golang下%不会把数变正
+        for sumMod < 0 {
+            sumMod += K
         }
-        //注意golang里%不会取正数
-        for P < 0 {
-            P += K
-        }
-        if num, ok := m[P]; ok {
-            result += num
-            m[P] = num + 1
-        } else {
-            m[P] = 1
-        }
+        result += modCount[sumMod]
+        modCount[sumMod]++
     }
-    //fmt.Println(m)
     return result
 }
