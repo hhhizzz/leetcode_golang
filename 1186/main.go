46950fd43b85db9688e689d73aeeb89833f50d08
xunix huang <qiwei_huang@qq.com>
2020-02-06 23:57:13 +0800

    modify 1186

--- 1186/main.go
+++ 1186/main.go
@@ -1,7 +1,5 @@
 package _1186
 
-import "math"
-
 func max(a, b int) int {
     if a > b {
         return a
@@ -16,19 +14,13 @@ func max3(a, b, c int) int {
 func maximumSum(arr []int) int {
     dp := make([]int, len(arr))
     dp2 := make([]int, len(arr))
-    result := math.MinInt32
-    for i := 0; i < len(arr); i++ {
-        if i == 0 {
-            dp[i] = arr[i]
-            if arr[i] > 0 {
-                dp2[i] = arr[i]
-            }
-            result = max(result, dp[i])
-        } else {
-            dp[i] = max(arr[i], dp[i-1]+arr[i])
-            dp2[i] = max(dp2[i-1]+arr[i], dp[i-1])
-            result = max3(result, dp[i], dp2[i])
-        }
+    dp[0] = arr[0]
+    dp2[0] = arr[0]
+    result := arr[0]
+    for i := 1; i < len(arr); i++ {
+        dp[i] = max(dp[i-1]+arr[i], arr[i])
+        dp2[i] = max(dp2[i-1]+arr[i], dp[i-1])
+        result = max3(result, dp[i], dp2[i])
     }
     return result
 }
