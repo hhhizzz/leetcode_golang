d112319242739226def2085e85162b5e7cd99640
xunix huang <qiwei_huang@qq.com>
2020-02-06 17:22:16 +0800

    upgrade 325

--- 325/main.go
+++ 325/main.go
@@ -1,7 +1,5 @@
 package _325
 
-import "fmt"
-
 //O(n^2)的算法 加了剪枝
 func maxSubArrayLen1(nums []int, k int) int {
     sums := make([]int, len(nums))
@@ -30,31 +28,27 @@ func maxSubArrayLen1(nums []int, k int) int {
 
 //用map存储可以降到O(n)
 func maxSubArrayLen(nums []int, k int) int {
-    sumPos := map[int]int{}
-    sums := make([]int, len(nums))
+    sum := make([]int, len(nums))
+    sumPos := map[int]int{0: -1}
 
-    result := 0
     for i := 0; i < len(nums); i++ {
         if i == 0 {
-            sums[i] = nums[i]
+            sum[i] = nums[i]
         } else {
-            sums[i] = nums[i] + sums[i-1]
+            sum[i] = sum[i-1] + nums[i]
         }
-        if _, ok := sumPos[sums[i]]; !ok {
-            sumPos[sums[i]] = i
+        if _, ok := sumPos[sum[i]]; !ok {
+            sumPos[sum[i]] = i
         }
     }
-    fmt.Println(sumPos)
-    for i := 0; i < len(nums); i++ {
-        if sums[i] == k {
-            result = i + 1
-        } else {
-            if pos, ok := sumPos[sums[i]-k]; ok && i-pos > result {
-                //fmt.Printf("%d %d\n",i,pos)
+    result := 0
+    for i := 0; i < len(sum); i++ {
+        diff := sum[i] - k
+        if pos, ok := sumPos[diff]; ok && pos < i {
+            if i-pos > result {
                 result = i - pos
             }
         }
     }
-
     return result
 }
