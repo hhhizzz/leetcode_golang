5379d5bb6d955cbbc9c6b120570418b91c0887e5
xunix huang <qiwei_huang@qq.com>
2020-06-09 23:25:31 +0800

    upgrade 7

--- 7/main.go
+++ 7/main.go
@@ -3,18 +3,21 @@ package _7
 import "math"
 
 func reverse(x int) int {
-    result := 0
-    for x != 0 {
-        current := x % 10
-        result += current
+	result := 0
+	for x != 0 {
+		current := x % 10
+		result += current
 
-        x /= 10
-        if x != 0 {
-            result *= 10
-        }
-    }
-    if result < math.MinInt32 || result > math.MaxInt32 {
-        return 0
-    }
-    return result
+		x /= 10
+		if x != 0 {
+			if result > math.MaxInt32/10 {
+				return 0
+			}
+			if result < math.MinInt32/10 {
+				return 0
+			}
+			result *= 10
+		}
+	}
+	return result
 }
