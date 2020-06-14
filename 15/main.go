e672185e583b8f975883aa379108295eb2c09944
xunix huang <qiwei_huang@qq.com>
2020-06-14 16:56:06 +0800

    upgrade 15

--- 15/main.go
+++ 15/main.go
@@ -1,5 +1,7 @@
 package _15
 
+import "sort"
+
 func qsort(nums []int) {
 	if len(nums) <= 1 {
 		return
@@ -46,38 +48,30 @@ func threeSum1(nums []int) [][]int {
 }
 
 func threeSum(nums []int) [][]int {
-	qsort(nums)
-	var result [][]int
+	sort.Ints(nums)
+	var res [][]int
 	for i := 0; i < len(nums)-2; i++ {
-		if i > 0 && nums[i] == nums[i-1] {
-			continue
+		for i > 0 && i < len(nums)-1 && nums[i] == nums[i-1] {
+			i++
 		}
-		left := i + 1
-		right := len(nums) - 1
-		for left < right {
-			total := nums[i] + nums[left] + nums[right]
-			if total > 0 {
-				right--
-				for left < right && nums[right] == nums[right+1] {
-					right--
+		for l, r := i+1, len(nums)-1; l < r; {
+			sum := nums[i] + nums[l] + nums[r]
+			if sum == 0 {
+				res = append(res, []int{nums[i], nums[l], nums[r]})
+				l++
+				for l < r && nums[l] == nums[l-1] {
+					l++
 				}
-			} else if total < 0 {
-				left++
-				for left < right && nums[left] == nums[left-1] {
-					left++
+				r--
+				for l < r && nums[r] == nums[r+1] {
+					r--
 				}
+			} else if sum < 0 {
+				l++
 			} else {
-				result = append(result, []int{nums[i], nums[left], nums[right]})
-				right--
-				for left < right && nums[right] == nums[right+1] {
-					right--
-				}
-				left++
-				for left < right && nums[left] == nums[left-1] {
-					left++
-				}
+				r--
 			}
 		}
 	}
-	return result
+	return res
 }
