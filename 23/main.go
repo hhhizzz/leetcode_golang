f47b86f563531442e624155d933e4a40c3435bb8
xunix huang <qiwei_huang@qq.com>
2020-06-16 22:24:19 +0800

    upgrade 23

--- 23/main.go
+++ 23/main.go
@@ -1,6 +1,8 @@
 package _23
 
-import "container/heap"
+import (
+	"container/heap"
+)
 
 /**
  * Definition for singly-linked list.
@@ -36,7 +38,7 @@ func (h *nodeHeap) Pop() interface{} {
 	return x
 }
 
-func mergeKLists(lists []*ListNode) *ListNode {
+func mergeKLists1(lists []*ListNode) *ListNode {
 	h := &nodeHeap{}
 	heap.Init(h)
 	front := &ListNode{}
@@ -53,6 +55,79 @@ func mergeKLists(lists []*ListNode) *ListNode {
 		if min.Next != nil {
 			heap.Push(h, min.Next)
 		}
+		current = current.Next
+	}
+	return front.Next
+}
+
+type mineHeap struct {
+	array []*ListNode
+}
+
+func (h *mineHeap) pop() *ListNode {
+	root := 0
+	last := len(h.array) - 1
+
+	result := h.array[root]
+	h.array[root], h.array[last] = h.array[last], h.array[root]
+	h.array = h.array[:len(h.array)-1]
+
+	parent := root
+
+	for {
+		child := parent*2 + 1
+		if child >= len(h.array) {
+			break
+		}
+		if rChild := child + 1; rChild < len(h.array) && h.array[rChild].Val < h.array[child].Val {
+			child = rChild
+		}
+		if h.array[child].Val < h.array[parent].Val {
+			h.array[parent], h.array[child] = h.array[child], h.array[parent]
+		}
+		parent = child
+	}
+
+	return result
+}
+
+func (h *mineHeap) push(node *ListNode) {
+	h.array = append(h.array, node)
+
+	child := len(h.array) - 1
+	parent := (child - 1) >> 1
+
+	for child != 0 && h.array[parent].Val > h.array[child].Val {
+		h.array[child], h.array[parent] = h.array[parent], h.array[child]
+		child = parent
+		parent = (child - 1) >> 1
+	}
+}
+
+func (h *mineHeap) nextNode() *ListNode {
+	if len(h.array) == 0 {
+		return nil
+	}
+	first := h.pop()
+	next := first.Next
+	if next != nil {
+		h.push(next)
+	}
+	return first
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	h := mineHeap{array: []*ListNode{}}
+	for _, list := range lists {
+		if list != nil {
+			h.push(list)
+		}
+	}
+	front := &ListNode{}
+	current := front
+
+	for next := h.nextNode(); next != nil; next = h.nextNode() {
+		current.Next = next
 		current = current.Next
 	}
 	return front.Next
--- 23/main_test.go
+++ 23/main_test.go
@@ -33,3 +33,52 @@ func TestMergeKLists(t *testing.T) {
 	result := mergeKLists([]*ListNode{list1, list2, list3})
 	println(toStr(result))
 }
+
+func TestMineHeap(t *testing.T) {
+	list1 := makeListNode([]int{4})
+	list2 := makeListNode([]int{1})
+	list3 := makeListNode([]int{2})
+	h := mineHeap{array: []*ListNode{}}
+
+	h.push(list1)
+	h.push(list2)
+	h.push(list3)
+
+	var popVal int
+	popVal = h.pop().Val
+	if popVal != 1 {
+		t.Errorf("expect %d != 1", popVal)
+	}
+
+	popVal = h.pop().Val
+	if popVal != 2 {
+		t.Errorf("expect %d != 2", popVal)
+	}
+
+	popVal = h.pop().Val
+	if popVal != 4 {
+		t.Errorf("expect %d != 4", popVal)
+	}
+
+	h.push(list1)
+	popVal = h.pop().Val
+	if popVal != 4 {
+		t.Errorf("expect %d != 4", popVal)
+	}
+
+	h.push(list1)
+	h.push(list2)
+	popVal = h.pop().Val
+	if popVal != 1 {
+		t.Errorf("expect %d != 1", popVal)
+	}
+
+	h.push(list1)
+	h.push(list2)
+	h.push(list3)
+	popVal = h.pop().Val
+	if popVal != 1 {
+		t.Errorf("expect %d != 1", popVal)
+	}
+
+}
