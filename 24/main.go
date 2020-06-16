c2a1e18c5f6cce5a446e8beea65d0a19605c9c02
xunix huang <qiwei_huang@qq.com>
2020-06-16 22:49:39 +0800

    add comment in 24

--- 24/main.go
+++ 24/main.go
@@ -11,6 +11,7 @@ func swapPairs(head *ListNode) *ListNode {
 	for pre != nil && pre.Next != nil && pre.Next.Next != nil {
 		first := pre.Next
 		second := first.Next
+		// pre -> first -> second ====> pre -> second -> first
 		pre.Next, first.Next, second.Next = second, second.Next, first
 		pre = first
 	}
