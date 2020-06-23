fe9c16ceb1eb76dc941d0457019f8c66bea0d601
xunix huang <qiwei_huang@qq.com>
2020-06-23 23:13:11 +0800

    upgrade 34

--- 34/main.go
+++ 34/main.go
@@ -1,42 +1,40 @@
 package _34
 
 func searchRange(nums []int, target int) []int {
-	if len(nums) == 0 {
-		return []int{-1, -1}
-	}
 	left := 0
-	right := len(nums) - 1
-	targetLeft := 0
-	targetRight := 0
-	for left < right-1 {
-		mid := left + ((right - left) >> 1)
-		if nums[mid] >= target {
+	right := len(nums)
+	res := []int{-1, -1}
+	for left < right {
+		mid := (left + right) >> 1
+		if target == nums[mid] {
+			if mid == 0 || nums[mid-1] != nums[mid] {
+				res[0] = mid
+				break
+			} else {
+				right = mid
+			}
+		} else if target < nums[mid] {
 			right = mid
 		} else {
-			left = mid
+			left = mid + 1
 		}
-	}
-	if nums[left] == target {
-		targetLeft = left
-	} else if nums[right] == target {
-		targetLeft = right
-	} else {
-		return []int{-1, -1}
 	}
 	left = 0
-	right = len(nums) - 1
-	for left < right-1 {
-		mid := left + ((right - left) >> 1)
-		if nums[mid] <= target {
-			left = mid
-		} else {
+	right = len(nums)
+	for left < right {
+		mid := (left + right) >> 1
+		if target == nums[mid] {
+			if mid == len(nums)-1 || nums[mid+1] != nums[mid] {
+				res[1] = mid
+				break
+			} else {
+				left = mid + 1
+			}
+		} else if target < nums[mid] {
 			right = mid
+		} else {
+			left = mid + 1
 		}
 	}
-	if nums[right] == target {
-		targetRight = right
-	} else if nums[left] == target {
-		targetRight = left
-	}
-	return []int{targetLeft, targetRight}
+	return res
 }
