1ef4a78cfbbeb281ee113818a05ebb7fbd334f78
xunix huang <qiwei_huang@qq.com>
2020-06-27 22:16:36 +0800

    add 47

--- 47/main.go
+++ 47/main.go
@@ -1,62 +1,32 @@
 package _47
 
-var array []int
-var used []bool
-var result [][]int
+import "sort"
 
-func dfs(i int, current *[]int) {
-	if used[i] {
-		return
-	}
-	used[i] = true
-	*current = append(*current, array[i])
-
-	defer func() {
-		used[i] = false
-		*current = (*current)[:len(*current)-1]
-	}()
-
-	if len(*current) == len(array) {
-		new := make([]int, len(array))
-		copy(new, *current)
-		result = append(result, new)
+func helper(nums []int, used map[int]bool, current []int, res *[][]int) {
+	if len(nums) == len(used) {
+		temp := make([]int, len(current))
+		copy(temp, current)
+		*res = append(*res, temp)
 	} else {
-		for j := 0; j < len(array); j++ {
-			if j > 0 && !used[j-1] && array[j] == array[j-1] {
+		for i := 0; i < len(nums); i++ {
+			if _, ok := used[i]; ok {
 				continue
 			}
-			dfs(j, current)
+			if i > 0 && !used[i-1] && nums[i] == nums[i-1] {
+				continue
+			}
+			current = append(current, nums[i])
+			used[i] = true
+			helper(nums, used, current, res)
+			current = current[:len(current)-1]
+			delete(used, i)
 		}
 	}
-}
-
-func qsort(nums []int) {
-	if len(nums) <= 1 {
-		return
-	}
-	pivot := 0
-	for i := 1; i < len(nums); i++ {
-		if nums[i] < nums[0] {
-			pivot++
-			nums[pivot], nums[i] = nums[i], nums[pivot]
-		}
-	}
-	nums[pivot], nums[0] = nums[0], nums[pivot]
-	qsort(nums[:pivot])
-	qsort(nums[pivot+1:])
 }
 
 func permuteUnique(nums []int) [][]int {
-	array = nums
-	qsort(array)
-	used = make([]bool, len(nums))
-	result = make([][]int, 0)
-	for i := 0; i < len(nums); i++ {
-		if i != 0 && array[i] == array[i-1] {
-			continue
-		}
-		current := make([]int, 0)
-		dfs(i, &current)
-	}
-	return result
+	var res [][]int
+	sort.Ints(nums)
+	helper(nums, map[int]bool{}, []int{}, &res)
+	return res
 }
