3e3c703eb9c75af6c5d385bc20d43cc522c425e7
xunix huang <qiwei_huang@qq.com>
2020-07-01 21:28:16 +0800

    upgrade 37

--- 37/main.go
+++ 37/main.go
@@ -55,6 +55,68 @@ func solve(board *[][]byte, i, j int) bool {
 	return false
 }
 
+// 第一次写
 func solveSudoku(board [][]byte) {
 	solve(&board, 0, 0)
+}
+
+func toBoxNumber(x, y int) int {
+	return x/3*3 + y/3
+}
+
+func nextEmpty(board [][]byte) (int, int) {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if board[i][j] == '.' {
+				return i, j
+			}
+		}
+	}
+	return -1, -1
+}
+
+func helper(board [][]byte, row [9][10]bool, col [9][10]bool, box [9][10]bool, i, j int) bool {
+	for number := 1; number <= 9; number++ {
+		if !col[j][number] && !row[i][number] && !box[toBoxNumber(i, j)][number] {
+			board[i][j] = byte(number) + '0'
+			row[i][number] = true
+			col[j][number] = true
+			box[toBoxNumber(i, j)][number] = true
+
+			nextI, nextJ := nextEmpty(board)
+			if nextI == -1 && nextJ == -1 {
+				return true
+			} else {
+				res := helper(board, row, col, box, nextI, nextJ)
+				if res {
+					return res
+				} else {
+					board[i][j] = '.'
+					row[i][number] = false
+					col[j][number] = false
+					box[toBoxNumber(i, j)][number] = false
+				}
+			}
+		}
+	}
+	return false
+}
+
+// 第二次尝试
+func solveSudoku2(board [][]byte) {
+	var row [9][10]bool
+	var col [9][10]bool
+	var box [9][10]bool
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if board[i][j] != '.' {
+				number := board[i][j] - '0'
+				row[i][number] = true
+				col[j][number] = true
+				box[toBoxNumber(i, j)][number] = true
+			}
+		}
+	}
+	i, j := nextEmpty(board)
+	helper(board, row, col, box, i, j)
 }
