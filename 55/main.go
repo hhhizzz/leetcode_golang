ad8e447985a982b9063cf05e26ffedfb93874f4d
xunix huang <qiwei_huang@qq.com>
2020-07-06 22:20:31 +0800

    upgrade 55

--- 55/main.go
+++ 55/main.go
@@ -1,5 +1,13 @@
 package _55
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// 方法1 倒着走看能不能走回去
 func canJump(nums []int) bool {
 	lastPos := len(nums) - 1
 	for i := len(nums) - 2; i >= 0; i-- {
@@ -8,4 +16,16 @@ func canJump(nums []int) bool {
 		}
 	}
 	return lastPos == 0
+}
+
+// 方法2 正着走尝试最远能走多远
+func canJump2(nums []int) bool {
+	end := 0
+	for i := 0; i < len(nums); i++ {
+		if i > end {
+			return false
+		}
+		end = max(end, i+nums[i])
+	}
+	return end >= len(nums)-1
 }
