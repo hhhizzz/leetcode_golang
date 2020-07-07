56d98821107944452cc22e7e90eefd9c7b7cef49
xunix huang <qiwei_huang@qq.com>
2020-07-07 22:22:24 +0800

    upgrade 52

--- 52/main.go
+++ 52/main.go
@@ -1,20 +1,26 @@
 package _52
 
-func dfs(row, col, pie, na int, cnt *int, n *int) {
-	if row >= *n {
-		*cnt++
+func dfs(row, col, slash, bSlash, n uint32, res *int) {
+	// 输入数据 0 表示可用状态
+	if row >= n {
+		*res += 1
 		return
 	}
-	bits := ^(col | pie | na) & ((1 << uint(*n)) - 1)
+	var bitsN uint32 = (1 << n) - 1
+	// bits中1表示可用状态
+	bits := ^(col | slash | bSlash) & bitsN
 	for bits != 0 {
-		p := bits & -bits
-		dfs(row+1, col|p, (pie|p)<<1, (na|p)>>1, cnt, n)
-		bits -= p
+		// 取出最低一位的1
+		lowBit := bits & -bits
+		// 每下降一行，slash往左一格，bSlash往右一格
+		dfs(row+1, col|lowBit, (slash|lowBit)<<1, (bSlash|lowBit)>>1, n, res)
+		// 去掉这个最低位，尝试下一个
+		bits -= lowBit
 	}
 }
 
 func totalNQueens(n int) int {
-	result := 0
-	dfs(0, 0, 0, 0, &result, &n)
-	return result
+	res := 0
+	dfs(0, 0, 0, 0, uint32(n), &res)
+	return res
 }
