0c2976354a55d41661945a01fdd8e6e0b01d7d8f
xunix huang <qiwei_huang@qq.com>
2020-07-07 21:48:33 +0800

    upgrade 51

--- 51/main.go
+++ 51/main.go
@@ -1,54 +1,41 @@
 package _51
 
-var num int
-var col map[int]bool
-var pie map[int]bool
-var na map[int]bool
-var result [][]string
-
-func dfs(row int, current *[]string) {
-	for i := 0; i < num; i++ {
-		_, colOK := col[i]
-		_, pieOK := pie[i+row]
-		_, naOK := na[i-row]
-		if !colOK && !pieOK && !naOK {
-			col[i] = true
-			pie[i+row] = true
-			na[i-row] = true
-			newCol := ""
-			for j := 0; j < num; j++ {
-				if j != i {
-					newCol += "."
-				} else {
-					newCol += "Q"
-				}
-			}
-			*current = append(*current, newCol)
-			if row == num-1 {
-				newResult := make([]string, num)
-				copy(newResult, *current)
-				result = append(result, newResult)
-			} else {
-				dfs(row+1, current)
-			}
-			delete(col, i)
-			delete(pie, i+row)
-			delete(na, i-row)
-			*current = (*current)[:row]
+func dfs(board [][]byte, i int, row []bool, slash []bool, bSlash []bool, result *[][]string) {
+	n := len(board)
+	if i == len(board) {
+		current := make([]string, n)
+		for x := 0; x < n; x++ {
+			current[x] = string(board[x])
+		}
+		*result = append(*result, current)
+	}
+	for j := 0; j < n; j++ {
+		if !row[j] && !slash[i+j] && !bSlash[i-j+n-1] {
+			board[i][j] = 'Q'
+			row[j] = true
+			slash[i+j] = true
+			bSlash[i-j+n-1] = true
+			dfs(board, i+1, row, slash, bSlash, result)
+			board[i][j] = '.'
+			row[j] = false
+			slash[i+j] = false
+			bSlash[i-j+n-1] = false
 		}
 	}
 }
 
-func totalNQueens(n int) int {
-	return len(solveNQueens(n))
-}
-
 func solveNQueens(n int) [][]string {
-	col = make(map[int]bool)
-	pie = make(map[int]bool)
-	na = make(map[int]bool)
-	result = [][]string{}
-	num = n
-	dfs(0, &[]string{})
+	board := make([][]byte, n)
+	for i := 0; i < n; i++ {
+		board[i] = make([]byte, n)
+		for j := 0; j < n; j++ {
+			board[i][j] = '.'
+		}
+	}
+	row := make([]bool, n)
+	slash := make([]bool, 2*n-1)
+	bSlash := make([]bool, 2*n-1)
+	var result [][]string
+	dfs(board, 0, row, slash, bSlash, &result)
 	return result
 }
