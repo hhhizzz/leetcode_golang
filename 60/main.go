8a7aeb5721fe71a00c2fef91473b9443416a7e81
xunix huang <qiwei_huang@qq.com>
2020-07-09 22:26:56 +0800

    upgrade 50

--- 60/main.go
+++ 60/main.go
@@ -1,40 +1,22 @@
 package _60
 
-import "strconv"
-
-var factorial []int
-
-func helper(array []int, k int) string {
-	var result string
-	if len(array) == 0 {
-		return result
-	}
-	if k <= 0 {
-		for i := range array {
-			result += strconv.Itoa(array[i])
-		}
-		return result
-	}
-	times := k / factorial[len(array)-1]
-	nextK := k % factorial[len(array)-1]
-
-	result += strconv.Itoa(array[times])
-	array = append(array[:times], array[times+1:]...)
-	return result + helper(array, nextK)
-}
-
+// 对于 n位长的数来说，有n!种排列，其中每个第一位有(n-1)!中排列，例如4位数，总共有24中排列，其中 1xxx有6种 2xxx有6种等等
+// 利用这个特性可以逐步求出第k个排列的每一位
 func getPermutation(n int, k int) string {
-	var array []int
-
-	factorial = make([]int, n+1)
-	array = make([]int, n)
-	factorial[0] = 1
-
+	var res []byte
+	var seq []byte
+	factorial := 1
 	for i := 1; i <= n; i++ {
-		factorial[i] = factorial[i-1] * i
+		seq = append(seq, byte(i)+'0')
+		factorial *= i
 	}
-	for i := range array {
-		array[i] = i + 1
+	k -= 1
+	for i := 0; i < n; i++ {
+		factorial /= n - i
+		times := k / factorial
+		res = append(res, seq[times])
+		seq = append(seq[:times], seq[times+1:]...)
+		k -= factorial * times
 	}
-	return helper(array, k-1)
+	return string(res)
 }
