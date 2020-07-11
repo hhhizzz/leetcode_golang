0d8e2fb723062cbfc1589a190ae7ee112d6843cb
xunix huang <qiwei_huang@qq.com>
2020-07-12 00:34:42 +0800

    upgrade 67

--- 67/main.go
+++ 67/main.go
@@ -1,42 +1,35 @@
 package _67
 
+func reverse(bytes []byte) {
+	for i := 0; i < len(bytes)>>1; i++ {
+		bytes[i], bytes[len(bytes)-1-i] = bytes[len(bytes)-1-i], bytes[i]
+	}
+}
+
 func addBinary(a string, b string) string {
-	var arrayA []byte
-	var arrayB []byte
-	for i := len(a) - 1; i >= 0; i-- {
-		arrayA = append(arrayA, a[i])
+	bytesA := []byte(a)
+	bytesB := []byte(b)
+
+	reverse(bytesA)
+	reverse(bytesB)
+
+	if len(bytesB) > len(bytesA) {
+		bytesA, bytesB = bytesB, bytesA
 	}
-	for i := len(b) - 1; i >= 0; i-- {
-		arrayB = append(arrayB, b[i])
-	}
-	if len(a) < len(b) {
-		arrayA, arrayB = arrayB, arrayA
-	}
+
 	var carry byte = 0
-	for i := 0; i < len(arrayA); i++ {
-		if i < len(arrayB) {
-			current := carry + (arrayA[i] - '0') + (arrayB[i] - '0')
-			if current >= 2 {
-				carry = 1
-			} else {
-				carry = 0
-			}
-			arrayA[i] = current%2 + '0'
-		} else {
-			current := carry + (arrayA[i] - '0')
-			if current >= 2 {
-				carry = 1
-			} else {
-				carry = 0
-			}
-			arrayA[i] = current%2 + '0'
+	for i := 0; i < len(bytesA); i++ {
+		current := bytesA[i] - '0' + carry
+		if i < len(bytesB) {
+			current += bytesB[i] - '0'
 		}
+		carry = current / 2
+		bytesA[i] = current%2 + '0'
 	}
 	if carry != 0 {
-		arrayA = append(arrayA, '1')
+		bytesA = append(bytesA, '1')
 	}
-	for i := 0; i < len(arrayA)/2; i++ {
-		arrayA[i], arrayA[len(arrayA)-i-1] = arrayA[len(arrayA)-i-1], arrayA[i]
-	}
-	return string(arrayA)
+
+	reverse(bytesA)
+	return string(bytesA)
 }
