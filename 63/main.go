44448d240be82fce57e6532bc827da799c6bf457
xunix huang <qiwei_huang@qq.com>
2020-07-11 11:54:38 +0800

    upgrade 63

--- 63/main.go
+++ 63/main.go
@@ -1,32 +1,28 @@
 package _63
 
 func uniquePathsWithObstacles(obstacleGrid [][]int) int {
-	m := len(obstacleGrid)
-	n := len(obstacleGrid[0])
-	graph := make([][]int, m)
-	obstacle := false
-	for i := 0; i < m; i++ {
-		graph[i] = make([]int, n)
-		if obstacle || obstacleGrid[i][0] == 1 {
-			obstacle = true
-			continue
-		} else {
-			graph[i][0] = 1
-		}
+	dp := make([][]int, len(obstacleGrid))
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(obstacleGrid[i]))
 	}
-	for j := 0; j < n; j++ {
-		if obstacleGrid[0][j] != 1 {
-			graph[0][j] = 1
-		} else {
-			break
-		}
+	if obstacleGrid[0][0] != 1 {
+		dp[0][0] = 1
 	}
-	for i := 1; i < m; i++ {
-		for j := 1; j < n; j++ {
-			if obstacleGrid[i][j] != 1 {
-				graph[i][j] = graph[i-1][j] + graph[i][j-1]
+	for i := 0; i < len(dp); i++ {
+		for j := 0; j < len(dp[i]); j++ {
+			if obstacleGrid[i][j] == 0 {
+				var current int
+				if i != 0 {
+					current += dp[i-1][j]
+				}
+				if j != 0 {
+					current += dp[i][j-1]
+				}
+				if i != 0 || j != 0 {
+					dp[i][j] = current
+				}
 			}
 		}
 	}
-	return graph[m-1][n-1]
+	return dp[len(obstacleGrid)-1][len(obstacleGrid[0])-1]
 }
