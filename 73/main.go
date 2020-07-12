e60f9487b13be69cbf4288ec859a1668f8284fde
xunix huang <qiwei_huang@qq.com>
2020-07-12 18:10:02 +0800

    upgrade 73

--- 73/main.go
+++ 73/main.go
@@ -1,24 +1,44 @@
 package _73
 
 func setZeroes(matrix [][]int) {
-	row := map[int]bool{}
-	col := map[int]bool{}
+	row1 := false
+	col1 := false
 	for i := 0; i < len(matrix); i++ {
 		for j := 0; j < len(matrix[i]); j++ {
 			if matrix[i][j] == 0 {
-				col[j] = true
-				row[i] = true
+				if i == 0 {
+					row1 = true
+				}
+				if j == 0 {
+					col1 = true
+				}
+				matrix[0][j] = 0
+				matrix[i][0] = 0
 			}
 		}
 	}
-	for r := range row {
-		for j := 0; j < len(matrix[r]); j++ {
-			matrix[r][j] = 0
+	for i := 1; i < len(matrix); i++ {
+		if matrix[i][0] == 0 {
+			for j := 0; j < len(matrix[i]); j++ {
+				matrix[i][j] = 0
+			}
 		}
 	}
-	for c := range col {
+	for j := 1; j < len(matrix[0]); j++ {
+		if matrix[0][j] == 0 {
+			for i := 0; i < len(matrix); i++ {
+				matrix[i][j] = 0
+			}
+		}
+	}
+	if row1 {
+		for j := 0; j < len(matrix[0]); j++ {
+			matrix[0][j] = 0
+		}
+	}
+	if col1 {
 		for i := 0; i < len(matrix); i++ {
-			matrix[i][c] = 0
+			matrix[i][0] = 0
 		}
 	}
 }
