2b1dd2ed77057085699ce0c35bef4d03a422c0e4
xunix huang <qiwei_huang@qq.com>
2020-07-12 20:53:27 +0800

    upgrade 74

--- 74/main.go
+++ 74/main.go
@@ -24,3 +24,20 @@ func searchMatrix(matrix [][]int, target int) bool {
 	}
 	return false
 }
+
+// 方法2 扫描过程也可以二分，但是估计m和n不算大，直接上速度应该差不多
+func searchMatrix2(matrix [][]int, target int) bool {
+	for i := 0; i < len(matrix); i++ {
+		if i == len(matrix)-1 || target < matrix[i+1][0] {
+			for j := 0; j < len(matrix[i]); j++ {
+				if matrix[i][j] == target {
+					return true
+				} else if matrix[i][j] > target {
+					break
+				}
+			}
+			break
+		}
+	}
+	return false
+}
