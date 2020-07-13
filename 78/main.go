417bfdc47f0ec94ffd0c0e27ed35d1f035a01c5b
xunix huang <qiwei_huang@qq.com>
2020-07-13 22:33:26 +0800

    upgrade 78

--- 78/main.go
+++ 78/main.go
@@ -1,15 +1,14 @@
 package _78
 
-func helper(nums, current []int, pos, n int, res *[][]int) {
-	current = append(current, nums[pos])
+func helper(nums, current []int, n int, res *[][]int) {
+	current = append(current, nums[0])
 	if len(current) == n {
 		temp := make([]int, n)
 		copy(temp, current)
 		*res = append(*res, temp)
 	} else {
-		diff := n - len(current)
-		for i := pos + 1; i+diff-1 < len(nums); i++ {
-			helper(nums, current, i, n, res)
+		for i := 1; i < len(nums); i++ {
+			helper(nums[i:], current, n, res)
 		}
 	}
 }
@@ -18,7 +17,7 @@ func subsets(nums []int) [][]int {
 	res := [][]int{{}}
 	for i := 1; i <= len(nums); i++ {
 		for j := 0; j < len(nums); j++ {
-			helper(nums, []int{}, j, i, &res)
+			helper(nums[j:], []int{}, i, &res)
 		}
 	}
 	return res
