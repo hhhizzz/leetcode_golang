fc00e14ed14e7e66ecba0c23dd7e8561e1c8d27b
xunix huang <qiwei_huang@qq.com>
2020-07-16 01:33:36 +0800

    upgrade 89

--- 89/main.go
+++ 89/main.go
@@ -2,24 +2,23 @@ package _89
 
 func grayCode(n int) []int {
 	m := map[int]bool{0: true}
-	result := []int{0}
-
+	res := []int{0}
 	var mask []int
+
 	num := 1
-	for i := 1; i <= n; i++ {
-		mask = append(mask, num)
+	for i := 0; i < n; i++ {
+		mask = append(mask, 1<<i)
 		num = num << 1
 	}
-
-	for i := 1; i < num; i++ {
+	for i := 0; i < num; i++ {
 		for _, ma := range mask {
-			current := result[len(result)-1] ^ ma
-			if _, ok := m[current]; !ok {
-				m[current] = true
-				result = append(result, current)
+			next := res[len(res)-1] ^ ma
+			if _, ok := m[next]; !ok {
+				res = append(res, next)
+				m[next] = true
 				break
 			}
 		}
 	}
-	return result
+	return res
 }
