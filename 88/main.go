e08efc4ae85ad998116ff7bb5e27aa0eafe7be89
xunix huang <qiwei_huang@qq.com>
2020-07-16 01:16:02 +0800

    upgrade 88

--- 88/main.go
+++ 88/main.go
@@ -4,29 +4,13 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	for i := m - 1; i >= 0; i-- {
 		nums1[i+n] = nums1[i]
 	}
-	l := n
-	r := 0
-	i := 0
-	for ; l < m+n && r < n; i++ {
-		if nums1[l] < nums2[r] {
-			nums1[i] = nums1[l]
-			l++
+	for i, j, pos := n, 0, 0; i < n+m || j < n; pos++ {
+		if j == n || (i != n+m && nums1[i] < nums2[j]) {
+			nums1[pos] = nums1[i]
+			i++
 		} else {
-			nums1[i] = nums2[r]
-			r++
-		}
-	}
-	if l == m+n {
-		for i < m+n {
-			nums1[i] = nums2[r]
-			r++
-			i++
-		}
-	} else {
-		for i < m+n {
-			nums1[i] = nums1[l]
-			l++
-			i++
+			nums1[pos] = nums2[j]
+			j++
 		}
 	}
 }
