455161aff95b6c1301840e1da7f35f6ba0622460
xunix huang <qiwei_huang@qq.com>
2020-07-16 22:12:18 +0800

    upgrade 78

--- 78/main.go
+++ 78/main.go
@@ -1,24 +1,21 @@
 package _78
 
-func helper(nums, current []int, n int, res *[][]int) {
-	current = append(current, nums[0])
-	if len(current) == n {
-		temp := make([]int, n)
-		copy(temp, current)
+import "sort"
+
+func helper(nums []int, pos int, path []int, res *[][]int) {
+	if pos == len(nums) {
+		temp := make([]int, len(path))
+		copy(temp, path)
 		*res = append(*res, temp)
-	} else {
-		for i := 1; i < len(nums); i++ {
-			helper(nums[i:], current, n, res)
-		}
+		return
 	}
+	helper(nums, pos+1, path, res)
+	helper(nums, pos+1, append(path, nums[pos]), res)
 }
 
 func subsets(nums []int) [][]int {
-	res := [][]int{{}}
-	for i := 1; i <= len(nums); i++ {
-		for j := 0; j < len(nums); j++ {
-			helper(nums[j:], []int{}, i, &res)
-		}
-	}
+	sort.Ints(nums)
+	var res [][]int
+	helper(nums, 0, []int{}, &res)
 	return res
 }
