f7bae4957108f6d5ea4b71c36153baf9e775cb5e
xunix huang <qiwei_huang@qq.com>
2020-07-19 21:36:51 +0800

    upgrade 72

--- 72/main.go
+++ 72/main.go
@@ -1,42 +1,45 @@
 package _72
 
-func min2(i, j int) int {
-	if i < j {
-		return i
+func min(a, b int) int {
+	if a < b {
+		return a
 	}
-	return j
+	return b
 }
 
-func min(i, j, k int) int {
-	return min2(min2(i, j), k)
-}
-
+// 动态规划 dp[i][j] 表示word1的前i个字符转换到word2的前j个字符最少需要的次数
 func minDistance(word1 string, word2 string) int {
-	if len(word1) == 0 {
-		return len(word2)
-	}
-	if len(word2) == 0 {
-		return len(word1)
-	}
-	dp := make([][]int, len(word1)+1)
-	dp[0] = make([]int, len(word2)+1)
-	for i := 0; i <= len(word1); i++ {
-		dp[i] = make([]int, len(word2)+1)
+	// 字符串动规题在前面垫一个空格会更方便操作
+	word1 = " " + word1
+	word2 = " " + word2
+	n := len(word1)
+	m := len(word2)
+
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, m)
+		// 初始化（下同） 很显然 零个字符要变成i个或者i个字符变成0个最快也需要i次
 		dp[i][0] = i
 	}
-	for j := 0; j <= len(word2); j++ {
+	for j := 0; j < m; j++ {
 		dp[0][j] = j
 	}
-	for i := 1; i <= len(word1); i++ {
-		ci := word1[i-1]
-		for j := 1; j <= len(word2); j++ {
-			cj := word2[j-1]
-			if ci == cj {
-				dp[i][j] = dp[i-1][j-1]
+
+	for i := 1; i < n; i++ {
+		for j := 1; j < m; j++ {
+			/*
+				考虑得到dp[i][j]有3种情况
+				1. word1增加一个字符或者word2减少一个字符，此时利用word1[:i]变换到word2[:j+1]的次数加一即可也就是dp[i-1][j]+1
+				2. word2减少一个字符或者word1增加一个字符，对应dp[i][j-1]+1
+				3. word1变换一个字符得到word[2] 此时看word1[i]与word2[j]是否匹配，如果匹配，那么也不需要变换，直接就是dp[i-1][j-1]，否则还需要+1
+			*/
+			dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1
+			if word1[i] == word2[j] {
+				dp[i][j] = min(dp[i-1][j-1], dp[i][j])
 			} else {
-				dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+				dp[i][j] = min(dp[i-1][j-1]+1, dp[i][j])
 			}
 		}
 	}
-	return dp[len(word1)][len(word2)]
+	return dp[n-1][m-1]
 }
