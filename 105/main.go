36d6ab9f949ad0f3130b7ef5e34ad6884521654c
xunix huang <qiwei_huang@qq.com>
2020-07-22 23:15:32 +0800

    upgrade 105

--- 105/main.go
+++ 105/main.go
@@ -7,19 +7,17 @@ type TreeNode struct {
 }
 
 func buildTree(preorder []int, inorder []int) *TreeNode {
-	if len(preorder) <= 0 {
+	if len(preorder) == 0 {
 		return nil
 	}
-	rootVal := preorder[0]
-	root := &TreeNode{rootVal, nil, nil}
-	for i, num := range inorder {
-		if num == rootVal {
-			inOrderLeft := inorder[:i]
-			inOrderRight := inorder[i+1:]
-			preOrderLeft := preorder[1 : 1+len(inOrderLeft)]
-			preOrderRight := preorder[1+len(inOrderLeft):]
-			root.Left = buildTree(preOrderLeft, inOrderLeft)
-			root.Right = buildTree(preOrderRight, inOrderRight)
+	value := preorder[0]
+	root := &TreeNode{Val: value}
+
+	for i := 0; i < len(inorder); i++ {
+		if inorder[i] == value {
+			root.Left = buildTree(preorder[1:1+i], inorder[:i])
+			root.Right = buildTree(preorder[1+i:], inorder[i+1:])
+			break
 		}
 	}
 	return root
