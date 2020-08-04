e31dbf80462d297963b16172ef1bbb2db7ef6242
xunix huang <qiwei_huang@qq.com>
2020-08-04 21:53:35 +0800

    upgrade 122

--- 122/main.go
+++ 122/main.go
@@ -1,11 +1,11 @@
 package _122
 
 func maxProfit(prices []int) int {
-	result := 0
-	for i := 0; i < len(prices)-1; i++ {
-		if prices[i+1] > prices[i] {
-			result += prices[i+1] - prices[i]
+	profit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			profit += prices[i] - prices[i-1]
 		}
 	}
-	return result
+	return profit
 }
