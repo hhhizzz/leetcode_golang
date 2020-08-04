ce5c01504608b5fd462e784f8e6e3c131f96c8fb
xunix huang <qiwei_huang@qq.com>
2020-08-04 21:10:46 +0800

    upgrade 118

--- 118/main.go
+++ 118/main.go
@@ -1,19 +1,15 @@
 package _118
 
 func generate(numRows int) [][]int {
-	result := make([][]int, numRows)
-	if numRows == 0 {
-		return result
-	}
-	result[0] = make([]int, 1)
-	result[0][0] = 1
-	for i := 1; i < numRows; i++ {
-		result[i] = make([]int, i+1)
-		result[i][0] = 1
-		result[i][i] = 1
-		for j := 1; j < i; j++ {
-			result[i][j] = result[i-1][j-1] + result[i-1][j]
+	res := make([][]int, numRows)
+	for i := 0; i < numRows; i++ {
+		for j := 0; j < i+1; j++ {
+			if j == 0 || j == i {
+				res[i] = append(res[i], 1)
+			} else {
+				res[i] = append(res[i], res[i-1][j-1]+res[i-1][j])
+			}
 		}
 	}
-	return result
+	return res
 }
