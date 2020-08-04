b2e756ee58817c4dba2aa3fb09d2d9ecc0d5c147
xunix huang <qiwei_huang@qq.com>
2020-08-04 21:26:38 +0800

    upgrade 119

--- 119/main.go
+++ 119/main.go
@@ -1,19 +1,14 @@
 package _119
 
 func getRow(rowIndex int) []int {
-	array := make([][]int, 2)
-	array[0] = make([]int, rowIndex+1)
-	array[1] = make([]int, rowIndex+1)
-	for i := 0; i <= rowIndex; i++ {
-		current := i % 2
-		last := (i - 1) % 2
-		for j := 0; j < i+1; j++ {
-			if j == 0 || j == i {
-				array[current][j] = 1
-			} else {
-				array[current][j] = array[last][j-1] + array[last][j]
-			}
+	res := []int{1}
+	for i := 1; i <= rowIndex; i++ {
+		next := []int{1}
+		for i := 1; i < len(res); i++ {
+			next = append(next, res[i]+res[i-1])
 		}
+		next = append(next, 1)
+		res = next
 	}
-	return array[rowIndex%2]
+	return res
 }
