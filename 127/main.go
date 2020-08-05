eaf85ca6676b2225ce993126d1f1086bd2701a43
xunix huang <qiwei_huang@qq.com>
2020-08-05 21:45:51 +0800

    upgrade 127

--- 127/main.go
+++ 127/main.go
@@ -1,67 +1,49 @@
 package _127
 
-func changed(word1, word2 string) bool {
-	if len(word1) != len(word2) {
-		return false
-	}
-	diff := 0
-	for i := 0; i < len(word1); i++ {
-		if word1[i] != word2[i] {
-			diff++
-			if diff > 1 {
-				return false
-			}
+import "fmt"
+
+func compare(a, b string) bool {
+	num := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			num++
 		}
 	}
-	return diff == 1
+	return num == 1
+}
+
+func find(word string, wordList []string) bool {
+	for _, w := range wordList {
+		if compare(w, word) {
+			return true
+		}
+	}
+	return false
 }
 
 func ladderLength(beginWord string, endWord string, wordList []string) int {
-	g := make([][]bool, len(wordList))
-	for i := range g {
-		g[i] = make([]bool, len(wordList))
-	}
-	end := -1
-	for i := 0; i < len(wordList); i++ {
-		if wordList[i] == endWord {
-			end = i
-		}
-		for j := i + 1; j < len(wordList); j++ {
-			if changed(wordList[i], wordList[j]) {
-				g[i][j] = true
-				g[j][i] = true
+	currentList := []string{beginWord}
+	result := 0
+	for len(wordList) != 0 {
+		var nextList []string
+		var leftList []string
+		result++
+		for _, word := range wordList {
+			if find(word, currentList) {
+				if word == endWord {
+					fmt.Println(word, currentList)
+					return result + 1
+				}
+				nextList = append(nextList, word)
+			} else {
+				leftList = append(leftList, word)
 			}
 		}
-	}
-	if end == -1 {
-		return 0
-	}
-	var queue []int
-	dist := make([]int, len(wordList))
-	for i := 0; i < len(wordList); i++ {
-		dist[i] = -1
-	}
-	for i := 0; i < len(wordList); i++ {
-		if beginWord == wordList[i] {
-			dist[i] = 1
+		currentList = nextList
+		if len(leftList) == len(wordList) {
+			break
 		}
-		if changed(beginWord, wordList[i]) {
-			queue = append(queue, i)
-			dist[i] = 2
-		}
-	}
-	for len(queue) != 0 {
-		current := queue[0]
-		queue = queue[1:]
-		if current == end {
-			return dist[current]
-		}
-		for i := 0; i < len(g[current]); i++ {
-			if g[current][i] && dist[i] == -1 {
-				dist[i] = dist[current] + 1
-				queue = append(queue, i)
-			}
-		}
+		wordList = leftList
 	}
 	return 0
 }
