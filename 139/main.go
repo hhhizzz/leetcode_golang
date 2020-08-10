9727b3054942f004b9e6a09ddf9885efec51df03
xunix huang <qiwei_huang@qq.com>
2020-08-10 23:47:54 +0800

    add 139

--- 139/main.go
+++ 139/main.go
@@ -16,3 +16,50 @@ func wordBreak(s string, wordDict []string) bool {
 	}
 	return canCut[len(s)]
 }
+
+type Trie struct {
+	isWord bool
+	next   map[byte]*Trie
+}
+
+func constructTrie(wordDict []string) *Trie {
+	root := &Trie{next: map[byte]*Trie{}}
+	for _, word := range wordDict {
+		current := root
+		for i := 0; i < len(word); i++ {
+			letter := word[i]
+			if next, ok := current.next[letter]; !ok {
+				current.next[letter] = &Trie{next: map[byte]*Trie{}}
+				current = current.next[letter]
+			} else {
+				current = next
+			}
+		}
+		current.isWord = true
+	}
+	return root
+}
+
+// 方法2：使用trie优化，可以使复杂度减小到O(MN) M为wordDict中最长的一个单词
+func wordBreak2(s string, wordDict []string) bool {
+	root := constructTrie(wordDict)
+	dp := make([]bool, len(s))
+	for i := 0; i < len(s); i++ {
+		current := root
+		if i != 0 && !dp[i-1] {
+			continue
+		} else {
+			for j := i; j < len(s); j++ {
+				if next, ok := current.next[s[j]]; ok {
+					current = next
+					if current.isWord {
+						dp[j] = true
+					}
+				} else {
+					break
+				}
+			}
+		}
+	}
+	return dp[len(s)-1]
+}
