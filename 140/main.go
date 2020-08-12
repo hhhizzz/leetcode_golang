27ff8a1b9ae7c04363495141606ebdd9dac2ffb2
xunix huang <qiwei_huang@qq.com>
2020-08-12 21:24:17 +0800

    add 140

--- 140/main.go
+++ 140/main.go
@@ -26,3 +26,58 @@ func wordBreaks(s string, wordDict []string, start int, dp map[int][]string) []s
 	dp[start] = result
 	return result
 }
+
+type Trie struct {
+	isWord bool
+	next   map[byte]*Trie
+}
+
+func constructTrie(wordDict []string) *Trie {
+	root := &Trie{next: map[byte]*Trie{}}
+	for _, word := range wordDict {
+		current := root
+		for i := 0; i < len(word); i++ {
+			if next, ok := current.next[word[i]]; ok {
+				current = next
+			} else {
+				current.next[word[i]] = &Trie{next: map[byte]*Trie{}}
+				current = current.next[word[i]]
+			}
+		}
+		current.isWord = true
+	}
+	return root
+}
+
+func dfs(s string, start int, root *Trie, cache map[int][]string) []string {
+	if result, ok := cache[start]; ok {
+		return result
+	}
+	current := root
+	var result []string
+	for i := start; i < len(s); i++ {
+		if next, ok := current.next[s[i]]; ok {
+			current = next
+			if current.isWord {
+				if i == len(s)-1 {
+					result = append(result, s[start:i+1])
+				} else {
+					nextResults := dfs(s, i+1, root, cache)
+					for _, nextResult := range nextResults {
+						result = append(result, s[start:i+1]+" "+nextResult)
+					}
+				}
+			}
+		} else {
+			break
+		}
+	}
+	cache[start] = result
+	return result
+}
+
+// 方法2 用trie树优化
+func wordBreak2(s string, wordDict []string) []string {
+	root := constructTrie(wordDict)
+	return dfs(s, 0, root, map[int][]string{})
+}
